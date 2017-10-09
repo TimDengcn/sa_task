7634aef0ddb2d385eec913bc06352e89feeb92aa
邓天迈 <dengtimmycn@gmail.com>
2017-10-09 14:39:11 +0800

    Signed-off-by: 邓天迈 <dengtimmycn@gmail.com>

--- Task2{ddl=10.9}.cpp
+++ Task2{ddl=10.9}.cpp
@@ -2,48 +2,109 @@
 #include <ctype.h>
 #include <malloc.h>
 
-
 void bu_sort(int *, int);
-
+void swapint(int *, int *);
+void rever(int *,int);
 
 int main() {
-	char tem[8] = "0";
+	char tem[10] = "0";
 	bool isnum = true;
 	int *nums;
 	int total = 0;
 
+	//*************************
+
 	nums = (int *)malloc(sizeof(int));
 
-	while (1) {
-		scanf_s("%s", tem, 8);
+	//检测是否为数字,是则继续输入,不是则跳出循环
 
-		for (int i = 0; tem[i] != '\0'; i++)
-			if (!isdigit(tem[i]))
+	while (1) {
+		scanf_s("%s", tem, 10);
+
+		for (int i = 0; *(tem+i) != '\0'; i++)
+			if (!isdigit(*(tem+i)))
 				isnum = false;
 
 		if (isnum == false)
 			break;
 		else
-			sscanf(tem, "%d", nums + (total++));
+			sscanf_s(tem, "%d", nums + (total++));
 
 		nums = (int *)realloc(nums, sizeof(int)*(total + 1));
 	}
 
+	//*************************
+
 	bu_sort(nums, total);
 
+	printf("\n\n此为排序后结果:\n");
+
 	for (int i = 0; i < total; i++) {
-		printf("%d\n", nums[i]);
+		printf("%d	", nums[i]);
 	}
+
+	rever(nums, total);
+
+	printf("\n\n此为倒置后结果:\n");
+
+	for (int i = 0; i < total; i++) {
+		printf("%d	", nums[i]);
+	}
+
+	//*************************
+
 	free(nums);
 }
 
+
+//正反冒泡+提前终止以减少运算量
+
 void bu_sort(int *nums, int total) {
-	for (int i = 0; i < total; i++)
-		for (int j = 0; j < total - i; j++) {
-			if (nums[j] < +nums[j + 1]) {
-				int tem = nums[j];
-				nums[j] = nums[j + 1];
-				nums[j + 1] = tem;
+	int indexs, i;
+	int lows = 0;
+	int high= total -1;
+	indexs = lows;
+
+	//*************************
+
+	while (lows < high) {
+		//正向冒泡
+		for (i = lows; i < high; i++) {
+			if (*(nums + i) > *(nums + i + 1)) {
+				swapint(nums + i, nums + i + 1);
+				indexs = i;
 			}
 		}
+
+		high = indexs;
+		//逆向冒泡
+		for (i = high; i > lows; i--) {
+			if (*(nums + i) < *(nums + i - 1)) {
+				swapint(nums + i, nums + i - 1);
+				indexs = i;
+			}
+		}
+
+		lows = indexs;
+	}
+
+}
+
+//交换
+
+void swapint(int *numa , int *numb) {
+	int tem = 0;
+	tem = *numa;
+	*numa = *numb;
+	*numb = tem;
+}
+
+//倒置
+
+void rever(int * nums,int total) {
+	for (int i = 0; i < total / 2; i++) {
+		int tem = *(nums + i);
+		*(nums + i) = *(nums + total - 1 - i);
+		*(nums + total - 1 - i) = tem;
+	}
 }
